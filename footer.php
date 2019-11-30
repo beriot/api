5e14a0187ce21f5eae722cf276842b3daf58965a
beriot <lucas.beriot@gmail.com>
2019-11-30 18:03:26 +0100

    add index

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!-- Footer -->
+<footer style="background-color: #FFC107; height: 50px;display: flex;justify-content: center;padding-top: 10px;">
+    <a href="" style="color:black;margin-right: 20px;">Mentions legales</a>
+    <p>©2019 LuPauRay</p>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,162 @@
+
+<?php include "head.php"?>
+
+
+<body>
+
+<?php include "header.php"?>
+
+<!-- Content -->
+
+<div class="container-fluid pl-0 pr-0">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/image6.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/image4.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/image3.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="container-fluid pl-0 pr-0" id="lifestyle">
+        <center><h2 style="font-size: 40px; padding-top: 40px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Lifestyle</h2></center>
+        <p style="font-size:23px; padding: 30px 75px; text-align: justify; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Les abeilles vivent en colonie. Elles
+            forment une société très organisée, un peu comme une grande entreprise.
+            Autour de la reine, dont la tâche unique est de pondre et pondre encore, jusqu’à 50 000 ouvrières s’activent
+            avec ardeur. Dans la ruche, seules les quelques centaines de faux-bourdons paressent ! Durant leur
+            existence, les abeilles exercent jusqu’à sept fonctions différentes : nettoyeuse, nourrice, architecte,
+            manutentionnaire, ventileuse, gardienne et butineuse.
+            elles vivent de 30 à 45 jours pour les abeilles au printemps et en été,
+            plusieurs mois pour celles qui naissent à l’automne et permettent à la colonie de survivre à l’hiver et
+            redémarrer le cycle.
+        </p>
+    </div>
+</div>
+
+
+
+
+
+
+
+<div class="container-fluid pl-0 pr-0" id="ruche">
+    <div class="row no-gutters">
+        <div class="col-12 col-md-6  pl0 pr0">
+            <img src="img/ruche.jpg" style="width: 100%">
+        </div>
+        <div class="col-12 col-md-6" style="background-color: #212529; color: white">
+            <center><h2 style="font-size: 40px; padding-top: 40px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">La Rûche</h2></center>
+            <p style="font-size: 23px; padding: 30px 70px 60px 70px; text-align: justify; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif; margin-bottom: 0px">Une ruche est une structure presque
+                fermée abritant une colonie d'abeilles. L’intérieur de la ruche est composé de rayons formés par des
+                cellules hexagonales de cire d'abeille. Les abeilles utilisent ces cellules pour le stockage de la
+                nourriture (miel et pollen), et pour le renouvellement de la population (œufs, larves et nymphes).
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid pl-0 pr-0" id="reine">
+    <div class="row no-gutters">
+        <div class="col-12 col-md-6">
+            <center><h2 style="font-size: 40px; padding-top: 40px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">La Reine</h2></center>
+            <p style="font-size: 23px; padding: 30px 70px 60px 70px; text-align: justify; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+                La gelée royale est une substance produite par les abeilles ouvrières, grâce à des glandes contenues
+                dans leur tête. Elle est gélatineuse, de couleur blanchâtre.
+                La gelée royale apporte à la larve une substance qui permet de modifier son ADN pour en faire une
+                abeille reine et non ouvrière.
+            </p>
+        </div>
+        <div class="col-12 col-md-6 pl0 pr0">
+            <img src="img/queen.jpg" style="width: 100%">
+        </div>
+    </div>
+
+    <div>
+        <div class="container-fluid pl-0 pr-0" id="cause">
+            <center><h2 style="font-size: 40px; padding-top: 40px;background-color: #212529; color: white; margin-bottom: 0; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Les Causes</h2></center>
+            <p style="font-size:23px; padding: 30px 75px; text-align: justify; background-color: #212529; color: white; margin-bottom: 0; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+                Nous avons été sensibilisés au rôle des abeilles dans l'écosystème. En effet, les abeilles favorisent la
+                pollinisation des plantes sauvages et cultivées et elles sont sensibles aux traitements phytosanitaires.
+                Depuis quelques années le sort des abeilles est devenu inquiétant : leur taux de surmortalité atteint 30
+                à
+                35 %, taux anormalement élevé qui atteint dans certains cas 50 % de pertes hivernales. Sur le banc des
+                accusés : multiplication des substances chimiques dans l'environnement, présence de parasites, effets
+                désastreux des monocultures, ravages du frelon asiatique, impact du changement climatique
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid pl-0 pr-0" id="progress" style="padding: 40px; margin-left: 13%; width: 80%">
+    <div class="use">
+        <p style="margin-bottom: 10px; font-size: 20px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+            Utilité de l'espèce :</p>
+    </div>
+    <div class="progress" style="width: 90%;">
+        <div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0"
+             aria-valuemax="100"></div>
+    </div>
+
+    <div class="danger" style="padding-top: 30px;">
+        <p style="margin-bottom: 10px; font-size: 20px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+            Risque d'extinction :</p>
+    </div>
+    <div class="progress" style="width: 90%;">
+        <div class="progress-bar bg-warning" role="progressbar" style="width: 80%" aria-valuenow="100" aria-valuemin="0"
+             aria-valuemax="100">
+        </div>
+    </div>
+
+    <div class="hiver" style="padding-top: 30px;">
+        <p style="margin-bottom: 10px; font-size: 20px;font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Taux
+            de mortalité durant l'hiver :</p>
+    </div>
+    <div class="progress" style="width: 90%;">
+        <div class="progress-bar bg-warning" role="progressbar" style="width: 50%" aria-valuenow="75" aria-valuemin="0"
+             aria-valuemax="100"></div>
+    </div>
+</div>
+
+<div class="galerie" id="galerie" style="background-color: #212529; color: white; padding: 40px;">
+    <center><h2 style="font-size: 40px; padding-top: 10px;padding-bottom: 40px;background-color: #212529; color: white; margin-bottom: 0; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Les abeilles en vidéos</h2></center>
+    <div class="galerieVideos" style="display: flex; justify-content: center">
+        <?php
+        $API_key    = 'AIzaSyDJLp29i0mJkck4imBSpmVqUh9Uh99V9lE';
+        $channelID  = 'UC4manpkVsRoBBrQFBQqVJbA';
+        $maxResults = 3;
+
+        $videoList = json_decode(file_get_contents(
+            'https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelID.
+            '&maxResults='.$maxResults.
+            '&key='.$API_key.''
+        ));
+
+        foreach($videoList->items as $item){
+            if(isset($item->id->videoId)){
+                echo '<div class="youtube-video" style="display:block; padding: 0 40px;">
+        <iframe width="380" height="220" src="https://www.youtube.com/embed/'.$item->id->videoId.'" frameborder="0" allowfullscreen></iframe>
+        <h2 style="font-size: 15px">'. $item->snippet->title .'</h2>
+    </div>';
+            }
+        }
+        ?>
+    </div>
+    <center><a href="up.php" class="btn btn-warning" style="margin-top: 15px">Ajouter une vidéo a son compte</a></center>
+</div>
+
+
+
+<?php include "footer.php"?>
